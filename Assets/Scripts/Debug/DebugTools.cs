10e1f484e5b74ce643353d2b40b36f75511e98e9
Chris Almonte <chrisalmonte@protonmail.com>
2024-02-15 17:07:37 -0600

    Add Weapon Test Level

--- Assets/Scripts/Debug/DebugTools.cs
+++ Assets/Scripts/Debug/DebugTools.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class DebugTools : MonoBehaviour
 {
     public MainWeapon testWeapon;
+    public MainWeapon testWeapon2;
+    public MainWeapon testWeapon3;
 
     private WeaponSystem weaponSystem;
 
@@ -13,6 +15,8 @@ public class DebugTools : MonoBehaviour
     {
         weaponSystem = GetComponent<WeaponSystem>();
         Invoke("giveWeapon", 5);
+        Invoke("giveWeapon2", 20);
+        Invoke("giveWeapon3", 50);
     }
 
     // Update is called once per frame
@@ -24,6 +28,15 @@ public class DebugTools : MonoBehaviour
     private void giveWeapon()
     {
         if (weaponSystem != null) weaponSystem.EquipSpecialMWeapon(testWeapon);
+    }
+    private void giveWeapon2()
+    {
+        if (weaponSystem != null) weaponSystem.EquipSpecialMWeapon(testWeapon2);
+    }
+    
+    private void giveWeapon3()
+    {
+        if (weaponSystem != null) weaponSystem.EquipSpecialMWeapon(testWeapon3);
     }
 
 }
